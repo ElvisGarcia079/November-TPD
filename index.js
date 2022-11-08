bff00145e9021fe45793fca6cbcbe41a0c9b9fe5
Elvis <elvisgarcia93@gmail.com>
2022-11-08 14:24:46 -0500

    Create a Heading 1 Element using the DOM in JS

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Playing with the Vanilla Document Object Model
 
+// Step 1
 let body = document.querySelector("body");
 console.log(body);
+
+// Step 2
+let createHeading = document.createElement("h1");
+
