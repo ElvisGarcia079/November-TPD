526e400643d8adeee36e69bb6e0463853f615815
Elvis <elvisgarcia93@gmail.com>
2022-11-08 14:26:00 -0500

    Successfully Add Heading 1 Element to HTML Page using Vanilla JS DOM

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 // Step 1
 let body = document.querySelector("body");
-console.log(body);
+
 
 // Step 2
 let createHeading = document.createElement("h1");
 
+// Step 3
+body.appendChild(createHeading);
+
+// Notice that the Body now contains the Heading 1 Element, although I never added that myself in the HTML.
+console.log(body);
