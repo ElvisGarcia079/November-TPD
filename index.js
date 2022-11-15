1627d30fee4f9ac54dec602358935de1ac5b4411
Elvis <elvisgarcia93@gmail.com>
2022-11-15 13:24:16 -0500

    Create Button Element and Type Attribute with Value of Submit using DOM

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ let body = document.querySelector("body");
 
 // Step 2
 let createHeading = document.createElement("h1");
+let createButton = document.createElement("button");
 
 // Step 3
 body.appendChild(createHeading);
+let buttonElement = body.appendChild(createButton);
+
+
+buttonElement.setAttribute("type", "submit");
+
+
+console.log("The button: ", buttonElement)
+
+
+
 
 // Notice that the Body now contains the Heading 1 Element, although I never added that myself in the HTML.
 console.log(body);
+
+
